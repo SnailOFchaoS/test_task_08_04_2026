c3563ab26cebefbeb6d47e2b9d6ab640209ab7ca
pdenisov <pdenisov@topcase.ru>
2026-04-10 15:56:25 +0300

    edit for gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path='/' element={<MainPage />} />
           <Route path='/enter' element={<EnterPage />} />
